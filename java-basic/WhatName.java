febea37db68dd00bb7001f530b199470bfd8529d
skehdnr <skehdnr1@naver.com>
2019-08-09 18:11:12 +0900

    190809 18:10

--- java-basic/WhatName.java
+++ java-basic/WhatName.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 class WhatName{
     public static void main (String[]args){
-        System.out.print("이름이뭐니e"); //
+        System.out.println("이름 뭐니?");
+        String name = "";
+        // 값을 입력받는 지점
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        System.out.println("이름은 "+ name + " 입니다");
+        System.out.println("몇살이에요?");
+        int age = 0;
+        age = scanner.nextInt();
+        System.out.print("나이는 "+age + " 살 입니다");
     }
 }
